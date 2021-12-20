d3862df9baac0ebf8d0ce4e04c22d9aef9473760
Anton Kolesnikov <anton.3dgraphics@gmail.com>
2021-12-21 00:35:27 +0200

    data files changed types to ts, tsx

---
+++ src/components/Flow/nodes/labels/input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Handle } from 'react-flow-renderer';
+
+const InputLabelComponent = ( { id, label, type } ) => {
+  return (
+    <div className='node-base__content'>
+      <Handle type='target' position='left' id={ id } className={ type } />
+      <div className='node-base__content__title' style={ { textAlign: 'left' } }>{label}</div>
+    </div>
+  );
+};
+
+export default InputLabelComponent;
